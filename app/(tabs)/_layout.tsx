efe97520563a415a3a61b351d4ef5a3b72a493f8
Prashant Singh <prashant.singh181@gmail.com>
2026-03-23 10:35:00 +0530

    - Added Color Tokens - Created Custom Bottom Navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,140 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { colors } from "@/constants/colors";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Tabs } from "expo-router";
+import {
+    Image,
+    ImageBackground,
+    ImageSourcePropType,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+const tabBarHeight = 52;
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+        tabBarShowLabel: false,
+        tabBarStyle: styles.tabBar,
+        tabBarItemStyle: styles.tabBarItem,
+        tabBarIconStyle: { width: "100%", height: "100%" },
+        tabBarButton: ({ children, onPress, onLongPress }) => (
+          <TouchableOpacity
+            onPress={onPress}
+            onLongPress={onLongPress ?? undefined}
+            style={{ flex: 1 }}
+          >
+            <View
+              style={{
+                alignItems: "center",
+                height: "100%",
+                justifyContent: "center",
+              }}
+            >
+              {children}
+            </View>
+          </TouchableOpacity>
+        ),
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ focused }) => (
+            <TabbarIcon focused={focused} icon={icons.home} text="Home" />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="search"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ focused }) => (
+            <TabbarIcon focused={focused} icon={icons.search} text="Search" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabbarIcon focused={focused} icon={icons.save} text="Saved" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <TabbarIcon focused={focused} icon={icons.person} text="Profile" />
+          ),
         }}
       />
     </Tabs>
   );
 }
+
+interface TabbarIconProps {
+  focused: boolean;
+  icon: ImageSourcePropType;
+  text: string;
+}
+
+function TabbarIcon({ focused, icon, text }: Readonly<TabbarIconProps>) {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        style={styles.tabIconBackground}
+      >
+        <Image
+          source={icon}
+          style={{ tintColor: colors.primary }}
+          tintColor="#151312"
+        />
+        <Text style={styles.tabIconText}>{text}</Text>
+      </ImageBackground>
+    );
+  } else {
+    return (
+      <View>
+        <Image source={icon} tintColor="#a8b5db" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  tabBar: {
+    marginBottom: 36,
+    marginHorizontal: 10,
+    height: tabBarHeight,
+    backgroundColor: "#0F0D23",
+    borderRadius: 50,
+    position: "absolute",
+    borderTopWidth: 0,
+    elevation: 0,
+  },
+  tabBarItem: {
+    height: tabBarHeight,
+  },
+  tabIconBackground: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+    borderRadius: 50,
+    width: "100%",
+    height: tabBarHeight,
+    overflow: "hidden",
+  },
+  tabIconText: {
+    color: colors.secondary,
+    fontSize: 14,
+    fontWeight: "600",
+  },
+});
---
+++ components/search-bar.tsx
@@ -0,0 +1,59 @@
+import { colors } from "@/constants/colors";
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, StyleSheet, TextInput, View } from "react-native";
+
+interface SearchBarProps {
+  placeholder?: string;
+  onPress?: () => void;
+  as?: "input" | "button";
+}
+
+const SearchBar = ({
+  placeholder = "Search...",
+  onPress,
+  as = "input",
+}: SearchBarProps) => {
+  return (
+    <View style={styles.searchContainer}>
+      <Image
+        source={icons.search}
+        style={styles.searchIcon}
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        style={styles.searchInput}
+        placeholder={placeholder}
+        placeholderTextColor="#a8b5db"
+        onPress={onPress}
+        editable={as === "input"}
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: colors.dark[200],
+    borderRadius: 999,
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+  },
+  searchIcon: {
+    height: 20,
+    width: 20,
+  },
+  searchInput: {
+    flex: 1,
+    marginLeft: 8,
+    color: "white",
+    padding: 0,
+    textAlignVertical: "center",
+    includeFontPadding: false,
+  },
+});
